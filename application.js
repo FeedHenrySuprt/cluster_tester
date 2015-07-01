0a69ccf6aa95f988cfae39eeb626519f88e9342b
Brian Dooley <brian.dooley@feedhenry.com>
2015-07-01 10:04:15 +0100

    tidy up

--- application.js
+++ application.js
@@ -6,23 +6,19 @@ var cluster = require('cluster');
 
 //console.log(cluster);
 if (cluster.isMaster) {
+	 // Create four workers
+	 for (var i = 0; i < 4; i++) {
+	    console.log('Creating worker ' + i);
+	    cluster.fork();
+	 }
 
-     // Count the machine's CPUs
-     //var cpuCount = require('os').cpus().length;
+	  Object.keys(cluster.workers).forEach(function(id) {
+	    console.log("I am running with ID : "+cluster.workers[id].process.pid);
+	  });
 
-     // Create four workers
-     for (var i = 0; i < 4; i++) {
-        console.log('Creating worker ' + i);
-        cluster.fork();
-     }
-
-  Object.keys(cluster.workers).forEach(function(id) {
-    console.log("I am running with ID : "+cluster.workers[id].process.pid);
-  });
-
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
-  });
+	  cluster.on('exit', function(worker, code, signal) {
+	    console.log('worker ' + worker.process.pid + ' died');
+	  });
 
  // Code to run if we're in a worker process
  } else {
